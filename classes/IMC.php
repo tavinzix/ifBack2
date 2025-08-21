f0fe0adc2d8dbd1c57ce65cb81ad87c28008fe49
Otávio Fagundes <otaviofariafagundes1@gmail.com>
2025-08-20 22:58:43 -0300

    atividade 1

--- classes/IMC.php
+++ classes/IMC.php
@@ -1,18 +1,32 @@
 <?php
 include_once "Pessoa.php";
 
-class IMC{
+class IMC
+{
 
-    public static function toString() { 
-            return self::class;//$this
-     }
-
-    public static function calc(Pessoa $objPessoa){
-        echo "Calculando o IMC de $objPessoa->nome\n";
-        return;
+    public static function toString()
+    {
+        return self::class; //$this
     }
 
-    public static function classifica(Pessoa $objPessoa){
-        return self::calc($objPessoa);
+    public static function calc(Pessoa $objPessoa)
+    {
+        return $objPessoa->peso / $objPessoa->altura ** 2;
+    }
+
+    public static function classifica(Pessoa $objPessoa) : string
+    {
+        $imc = self::calc($objPessoa);
+
+        if ($imc < 18.5) {
+            return "Abaixo do peso";
+           
+        } elseif (($imc >= 18.5) && ($imc < 24.9)) {
+            return "Saudavel";
+        } elseif (($imc >= 25) && ($imc < 29.9)) {
+            return "Sobrepeso";
+        } else {
+           return "Obesidade";
+        }
     }
 }
